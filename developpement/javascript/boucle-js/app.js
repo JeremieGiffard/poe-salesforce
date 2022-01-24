9f411e798a272a116f103955fc983061b0d7441b
jeremie giffard <e132784f@gmail.com>
2022-01-24 11:27:18 +0100

    TP5-TP6-TP7

--- developpement/javascript/boucle-js/app.js
+++ developpement/javascript/boucle-js/app.js
@@ -1,11 +1,6 @@
-/*
-
-// variables
-
-const mail = prompt(`email`);
-console.log(mail);
-const mdp = prompt(`mot de pass`);
-console.log(mdp);
+// TP5
+const mail = prompt(`email`); //console.log(mail);
+const mdp = prompt(`mot de pass`); //console.log(mdp);
 let tentativeMail;
 let tentativeMdp;
 let i = 0;
@@ -15,16 +10,53 @@ while ((tentativeMail != mail || tentativeMdp != mdp) && i < 5) {
   tentativeMdp = prompt("verifier mdp");
   i++;
   if (tentativeMail != mail || tentativeMdp != mdp) {
-    alert(`do not match!! il vous reste ${5 - i} tentatives`);
+    alert(
+      `do not match!! il vous reste ${5 - i} tentatives avant d'etre bloqué`
+    );
   } else {
     alert("log_in");
   }
 }
-*/
 
+/*
+//TP6
 let number = prompt("entrez un nombre");
 
-while (isNaN(number) || number < 1 || number > 3) {
-  number = prompt("entrez un nombre");
+while (isNaN(number) || number <= 1 || number >= 3) {
+  number = prompt("tentez encore un nombre");
 }
-console.log(number);
+console.log(number);*/
+/*
+//TP7
+let guessNumber = prompt("entrez un nombre");
+
+while (isNaN(guessNumber) || guessNumber < 10 || guessNumber > 20) {
+  
+  if (isNaN(guessNumber)) {
+    alert("input non valide : veuillez rentrer un nombre (exemple : 50) ");
+    guessNumber = prompt("entrez un nombre");
+  } 
+  
+  
+  else if (guessNumber < 10) {
+    alert(`${guessNumber} : trop petit, essaye encore`);
+    guessNumber = prompt("entrez un nombre");
+  } else {
+    alert(`${guessNumber} : trop grand`);
+    guessNumber = prompt("entrez un nombre");
+  }
+}
+
+if (guessNumber >= 10 && guessNumber <= 20) {
+  alert(`réussi`);
+}
+*/
+/*
+//TP8
+
+const number = parseInt(prompt("choisi un nombre"));
+
+for (let index = 0; index <= 10; index++) {
+  const element = array[index];
+}
+*/
